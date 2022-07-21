42bc9b90920732185a3ed6070b25e3ff5f5c93fd
Harish Sivaram <97385071+HarishSiva05@users.noreply.github.com>
2022-07-21 23:07:36 +0530

    Initial Commit with FLights Array added

---
+++ app.js
@@ -0,0 +1,28 @@
+const tablebody = document.getElementById('t-body')
+
+let flights = [
+    {
+        time: "10:00",
+        destination: 'LAX',
+        flights:'BJ 007',
+        remarks:'SCHEDULED'
+    },
+    {
+        time: "12:00",
+        destination: 'ABU',
+        flights:'BJ 0R7',
+        remarks:'DELAYED'
+    },
+    {
+        time: "1:00",
+        destination: 'SDI',
+        flights:'BJ 007',
+        remarks:'SCHEDULED'
+    },
+    {
+        time: "09:00",
+        destination: 'MJI',
+        flights:'BJ 007',
+        remarks:'SCHEDULED'
+    },
+]
